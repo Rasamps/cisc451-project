b02e5b6a8ece3dc2fd1dc3d8cb79bf4bdbf83a6e
Emma Ritcey <50669185+emmaritcey@users.noreply.github.com>
2020-11-27 17:10:38 -0500

    Add files via upload

--- project/regression_code_emma/run.py
+++ project/regression_code_emma/run.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import modeling as md
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error
+
 
 def main():
     df = pd.read_csv('master_with_aq.csv', header = 0)
@@ -33,30 +33,14 @@ def main():
     #get indices of countries not in training set to use for test set
     test_indices = np.setdiff1d(nums, np.sort(train_indices))
     
-    all_Y_actual = np.zeros((len(test_indices),19))
-    all_Y_pred = np.zeros((len(test_indices),19))
-    for i in range(0,len(test_indices)):
-        Y_actual, Y_pred = md.fit_models(i, test_indices, pm25, df, models)
-        all_Y_actual[i,:] = Y_actual
-        all_Y_pred[i,:] = Y_pred
-    
-    rms1 = np.sqrt(mean_squared_error(all_Y_actual[0,:],all_Y_pred[0,:]))
-    rms2 = np.sqrt(mean_squared_error(all_Y_actual[1,:],all_Y_pred[1,:]))
-    rms3 = np.sqrt(mean_squared_error(all_Y_actual[2,:],all_Y_pred[2,:]))
-    rms4 = np.sqrt(mean_squared_error(all_Y_actual[3,:],all_Y_pred[3,:]))
-    rms5 = np.sqrt(mean_squared_error(all_Y_actual[4,:],all_Y_pred[4,:]))
-    rms6 = np.sqrt(mean_squared_error(all_Y_actual[5,:],all_Y_pred[5,:]))
+    all_Y_actual, all_Y_pred = md.model_driver(test_indices, pm25, df, models)
 
-    #print(Y_actual)
-    #print(Y_pred)
-    print('Linear Regression:')
-    print(rms1)
-    print(rms2)
-    print(rms3)
-    print(rms4)
-    print(rms5)
-    print(rms6)
-    #calculate rmse for each 
+    
+    
+    #calculate rmse to evaluate the quality of each of the models predictions
+    md.model_evaluation(all_Y_actual, all_Y_pred)
+
+    
 
  
 main()
